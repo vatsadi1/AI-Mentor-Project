803176c9594c497902f9df7c48ee0695dc811675
Aditya <vatsaditya9060@gmail.com>
2026-06-25 02:32:58 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import RoadmapPage from "./features/Roadmaps/pages/Home";
-<<<<<<< HEAD
 import InterviewPage from "./features/Interview/pages/Home";
-=======
 import ResumePage from "./features/Resume/pages/Home";
->>>>>>> 50874ad5b44a12740509fa907b7f809362b852c1
 
 export default function App() {
   return (
@@ -13,11 +10,10 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/roadmap" element={<RoadmapPage />} />
-<<<<<<< HEAD
+
         <Route path="/interview" element={<InterviewPage />} />
-=======
+
         <Route path="/resume" element={<ResumePage />} />
->>>>>>> 50874ad5b44a12740509fa907b7f809362b852c1
       </Routes>
     </BrowserRouter>
   );
